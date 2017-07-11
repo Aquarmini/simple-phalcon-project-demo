e4d002c380ffdbc1ac2e6eac868c2ac702b62c02
李铭昕 <715557344@qq.com>
2017-07-11 13:43:39 +0800

    * 复制aopSdk中的curl方法

--- app/library/Alipay/AlipayClient.php
+++ app/library/Alipay/AlipayClient.php
@@ -175,7 +175,7 @@ class AlipayClient
         $data['sign'] = md5(http_build_query($data));
         $data['sign_type'] = 'MD5';
 
-        return $this->aopClient->curl($gatway_url, $data);
+        return $this->curl($gatway_url, $data);
     }
 
     public function sign($data)
@@ -187,6 +187,75 @@ class AlipayClient
     {
         $result = $this->aopClient->rsaCheckV1($data, $this->aliPublicKey, $this->signType);
         return $result;
+    }
+
+    protected function curl($url, $postFields = null)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_FAILONERROR, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        $postBodyString = "";
+        $encodeArray = Array();
+        $postMultipart = false;
+
+
+        if (is_array($postFields) && 0 < count($postFields)) {
+
+            foreach ($postFields as $k => $v) {
+                if ("@" != substr($v, 0, 1)) //判断是不是文件上传
+                {
+
+                    $postBodyString .= "$k=" . urlencode($this->aopClient->characet($v, $this->postCharset)) . "&";
+                    $encodeArray[$k] = $this->aopClient->characet($v, $this->postCharset);
+                } else //文件上传用multipart/form-data，否则用www-form-urlencoded
+                {
+                    $postMultipart = true;
+                    $encodeArray[$k] = new \CURLFile(substr($v, 1));
+                }
+
+            }
+            unset ($k, $v);
+            curl_setopt($ch, CURLOPT_POST, true);
+            if ($postMultipart) {
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $encodeArray);
+            } else {
+                curl_setopt($ch, CURLOPT_POSTFIELDS, substr($postBodyString, 0, -1));
+            }
+        }
+
+        if ($postMultipart) {
+
+            $headers = array('content-type: multipart/form-data;charset=' . $this->postCharset . ';boundary=' . $this->getMillisecond());
+        } else {
+
+            $headers = array('content-type: application/x-www-form-urlencoded;charset=' . $this->postCharset);
+        }
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+
+        $reponse = curl_exec($ch);
+
+        if (curl_errno($ch)) {
+
+            throw new Exception(curl_error($ch), 0);
+        } else {
+            $httpStatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            if (200 !== $httpStatusCode) {
+                throw new Exception($reponse, $httpStatusCode);
+            }
+        }
+
+        curl_close($ch);
+        return $reponse;
+    }
+
+    protected function getMillisecond()
+    {
+        list($s1, $s2) = explode(' ', microtime());
+        return (float)sprintf('%.0f', (floatval($s1) + floatval($s2)) * 1000);
     }
 
 }
