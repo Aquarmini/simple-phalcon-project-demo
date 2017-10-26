0ff71407f918c39ebab70c68e4060a355e2b9ca2
李铭昕 <715557344@qq.com>
2017-10-26 17:02:45 +0800

    * 修改算法

--- app/controllers/Test/SignController.php
+++ app/controllers/Test/SignController.php
@@ -15,14 +15,19 @@ class SignController extends Controller
         unset($params['_url']);
         $json = $this->request->getJsonRawBody(true) ?? [];
         $params = array_merge($params, $json);
-        $params['xappmac'] = $this->request->getHeader('X-APP-MAC');
-        $params['xappversion'] = $this->request->getHeader('X-APP-VERSION');
-        $params['xapptoken'] = $this->request->getHeader('X-APP-TOKEN');
-        $params['xappplatform'] = $this->request->getHeader('X-APP-PLATFORM');
-        $params['xappkey'] = $this->request->getHeader('X-APP-KEY');
-        $params['xappnonce'] = $this->request->getHeader('X-APP-NONCE'); // 0
-        $params['xappuuids'] = $this->request->getHeader('X-APP-UUIDS'); // 1
-        $params['xapptimes'] = $this->request->getHeader('X-APP-TIMES'); // 2
+        $headers = $this->request->getHeaders();
+        foreach ($headers as $key => $val) {
+            $headers[strtoupper($key)] = $val;
+        }
+
+        $params['xappmac'] = $headers['X-APP-MAC'] ?? null;
+        $params['xappversion'] = $headers['X-APP-VERSION'] ?? null;
+        $params['xapptoken'] = $headers['X-APP-TOKEN'] ?? null;
+        $params['xappplatform'] = $headers['X-APP-PLATFORM'] ?? null;
+        $params['xappkey'] = $headers['X-APP-KEY'] ?? null;
+        $params['xappnonce'] = $headers['X-APP-NONCE'] ?? null; // 0
+        $params['xappuuids'] = $headers['X-APP-UUIDS'] ?? null; // 1
+        $params['xapptimes'] = $headers['X-APP-TIMES'] ?? null; // 2
 
         $sign = $this->request->getHeader('X-APP-SIGN');
 
